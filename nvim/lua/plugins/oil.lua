0347045e0d105ea74d91f65390af4c39a5ea8ed5
David Dickson <davidkdickson@gmail.com>
2026-03-07 15:48:49 -0700

    fix(nvim): Restore default Oil keymaps by removing custom keymaps override

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,9 +5,6 @@ return {
     view_options = {
       show_hidden = true,
     },
-    keymaps = {
-      ["-"] = "actions.parent",
-    },
   },
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "Open file explorer" },
