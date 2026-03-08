8cd50029f7968729b109a5cdaba03fc295b665c8
David Dickson <davidkdickson@gmail.com>
2026-03-07 23:41:57 -0700

    fix(nvim): Load LSP config eagerly instead of waiting for InsertEnter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,7 +15,11 @@ return {
     },
   },
   {
-    "hrsh7th/cmp-nvim-lsp",
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
